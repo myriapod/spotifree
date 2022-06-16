fe51ea416933216bc9c0bbc17b191ede50529bd0
jenny973 <jenny.fung-fong-you@labom2iformation.fr>
2022-06-16 09:33:25 +0200

    modif aborescence

---
+++ INTERFACE/website/views.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+
+    if request.method == 'GET':
+        test3 = request.args.get('q')
+        print('recherche')
+        print(test3)
+    else:
+        test2 = request.form.get('postTest')
+        print('recherche')
+        print(test2)
+    return render_template("home.html", user=current_user,test=test2)
+
+
+# @views.route('/delete-note', methods=['POST'])
+# def delete_note():
+#     note = json.loads(request.data)
+#     noteId = note['noteId']
+#     note = Note.query.get(noteId)
+#     if note:
+#         if note.user_id == current_user.id:
+#             db.session.delete(note)
+#             db.session.commit()
+
+#     return jsonify({})
+
