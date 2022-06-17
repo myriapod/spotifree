f096a80a9b7d7d4292b907b40aace2fab75adbc3
jenny973 <106160854+jenny973@users.noreply.github.com>
2022-06-17 12:06:11 +0200

    Update __init__.py
    ajout commentaires

--- INTERFACE/website/__init__.py
+++ INTERFACE/website/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
+# Fonction permettant la configuration de Flask et création d'une BDD locale user
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
@@ -30,13 +30,14 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    # Récupération de l'id du user
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
     return app
 
-
+# Fonction permettant de créer une bdd locale (database.db) si elle n'existe pas déjà
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
