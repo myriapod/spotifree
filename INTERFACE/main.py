80ae52f6bf44ced10308b62b4e5a7f372ec74b30
jenny973 <106160854+jenny973@users.noreply.github.com>
2022-06-17 12:07:22 +0200

    Update main.py
    ajout commentaires

--- INTERFACE/main.py
+++ INTERFACE/main.py
@@ -1,3 +1,5 @@
+# Après avoir lancé le flask en local, démarré le script main.py avec python3
+# Permet la connexion sur la page html spotifree
 from INTERFACE.website import create_app
 
 app = create_app()
