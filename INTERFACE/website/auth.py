28338c9b70430bd0af39309d217b81aa1f9b8ba1
jenny973 <jenny.fung-fong-you@labom2iformation.fr>
2022-06-17 10:35:00 +0200

    ajout commentaires partie flask

--- INTERFACE/website/auth.py
+++ INTERFACE/website/auth.py
@@ -1,3 +1,5 @@
+# Script permettant l'inscription, la connexion et la déconnexion à Spotifree
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,18 +12,22 @@ from SERVEUR.classes_srv import serveur_BDD
 
 auth = Blueprint('auth', __name__)
 
-
+# Fonction permettant de se connecter au compte Spotifree
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        # Récupération de l'username et du password tapés sur la page
         username = request.form.get('username')
         password = request.form.get('password')
         
+        # Check avec le username stocké dans la BDD locale (faite avec flask : database.db) lors de l'inscription
         flask_user = User.query.filter_by(first_name=username).first()
         
+        # Import de la BDD user mariadb et check si l'user existe également
         BDD = serveur_BDD(username, password)
         exists = BDD.check_user()
         
+        # Si les deux checks sont concluants -> succès de connexion et redirection à la page home.html
         if exists and flask_user:
             flash('Logged in successfully!', category='success')
             login_user(flask_user, remember=True)
@@ -32,23 +38,26 @@ def login():
             #    return redirect(url_for('views.home'))
             #else:
             #    flash('Incorrect password, try again.', category='error')
+            
+        # Sinon -> erreur, pas de connexion
         else:
             flash('User does not exist or error with password.', category='error')
 
     return render_template("login.html", user=current_user)
 
-
+# Fonction déconnexion
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-
+# Fonction sign_up pour faire inscrire un compte à Spotifree
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        first_name = request.form.get('firstName')
+        # On récupère le nom, le mot de passe et la confirmation du mot de passe (password2) et stockage dans bdd user locale (flask) et bdd user mariadb
+        first_name = request.form.get('firstName') 
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
@@ -57,14 +66,19 @@ def sign_up():
         BDD = serveur_BDD(first_name, password1)
         exists = BDD.check_user()
         
+        # Si le nom d'utilisateur rentré existe déjà -> erreur
         if exists:
             flash('Username already exists.', category='error')
+        # Nom utilisateur doit avoir plus de 2 caractères
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
+        # si password1 différent de password2 -> erreur
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
+        # password doit avoir plus de 7 caractères
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
+        # Si conditions sont remplies -> création d'un nouveau compte
         else:
             BDD.sign_up()
             # on crée l'utilisateur sur une bdd locale pour pouvoir utiliser login_user de flask
--- INTERFACE/website/views.py
+++ INTERFACE/website/views.py
@@ -11,7 +11,7 @@ import os
 
 views = Blueprint('views', __name__)
 
-
+# Une fois la connexion du user à son compte réussie -> redirection à la page home.html
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
@@ -21,24 +21,35 @@ def home():
         for user_obj in result.scalars():
             password = user_obj.password
         
+        # Instanciation de la BDD par rapport à l'user 
         BDD = serveur_BDD(current_user.first_name, password)
+        # On vérifie que l'user existe dans la table user 
         BDD.check_user()
+        # Récupération du user id
         BDD.log_in()
+        # Connexion à la BDD de l'user 
         BDD.connection_user()
-    
+
+        # Vérification s'il y a des amis dans la BDD amis
         friends = 0
         if BDD.user_friends:
             friends = BDD.user_friends
 
         # chercher les musiques
         if request.method == 'GET':
+            # Récupère recherche de la musique
             recherche = request.args.get('recherche')
+            # Récupère le nom d'un ami dans la barre de recherche
             friend_search = request.args.get('friend-search')
             
+            # Cherche le nom d'un ami dans la BDD (si l'utilisateur existe ou pas)
             find_friend = BDD.find_friend(friend_search)
+            # S'il trouve un ami, on récupère l'id de l'ami 
             if find_friend:
                 friend_id = find_friend[0][0]
+                # ajout id de l'ami dans la table ami de la BDD
                 BDD.add_friends(friend_id)
+            # Si on a quand même fait une recherche d'ami 
             elif friend_search:
                 flash(f"Utilisateur {friend_search} non trouvé.")
         else:
@@ -50,6 +61,7 @@ def home():
             download_link = request.form['submit_button']
             try:
                 flash('Musique en cours de téléchargement...')
+                # le téléchargement se fait via os.system dans le terminal 
                 os.system(f"spotdl {download_link}")
             except OSError as err:
                 print(err)
@@ -58,18 +70,7 @@ def home():
         return render_template("home.html", user=current_user, recherche=recherche, resultats=resultats, friends=friends,
                             friend_search=friend_search)
 
+    # Première connexion -> redirection vers le login (erreur mariadb)
     except IndexError:
         return redirect(url_for('auth.login'))
-
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():
-#     note = json.loads(request.data)
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-
-#     return jsonify({})
 
