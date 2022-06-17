0087c45d0b08a0207c1e2220336fd88def1ddd9a
jenny973 <106160854+jenny973@users.noreply.github.com>
2022-06-17 12:04:34 +0200

    Update models.py
    ajout commentaires

--- INTERFACE/website/models.py
+++ INTERFACE/website/models.py
@@ -1,29 +1,12 @@
+# Classe User : permet de savoir sous quelle forme sont stockés les users inscrits dans la database.db (bdd user locale)
+# La classe est rappelée lors du check pour la connexion au compte
+
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     
-    
-class Music(db.Model):
-    track_id = db.Column(db.Integer, primary_key=True)
-    artist_name = db.Column(db.String(150))
-    album_name = db.Column(db.String(150))
-    track_name = db.Column(db.String(150))
-    track_number = db.Column(db.Integer)
-    spotify_link = db.Column(db.String(150))
-
-# Exemple de données musical.
-# {
-#     "track_id": 1,
-#     "artist_name": "Artist Vs Poet",
-#     "album_name": "Medicine",
-#     "track_name": "Fresh",
-#     "track_number": 1,
-#     "spotify_link": "https://open.spotify.com/artist/3kYFawNQVZ00FQbgs4rVBe"
-# }
